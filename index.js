d6526073c346360f5fcb5bb89578f244a87edc6c
Adrian Lugo <misteralugo@gmail.com>
2021-12-14 14:57:27 -0600

    mobile style sizing and buttons

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ async function getWord() {
     let data = await response.json()
     console.log(data[0].hwi.prs[0].sound.audio)
     const abacavirBaseFilename = data[0].hwi.prs[0].sound.audio
-    
-    // const containerGame = document.getElementById("container-game")
-    // containerGame.innerHTML += `
-    // <audio autoplay><source src="https://media.merriam-webster.com/audio/prons/en/us/mp3/a/${abacavirBaseFilename}.mp3"></audio>
-    // `
 
     let sound = new Audio(`https://media.merriam-webster.com/audio/prons/en/us/mp3/a/${abacavirBaseFilename}.mp3`)
     console.log(sound.src)
@@ -19,4 +14,5 @@ async function getWord() {
 
 
 
-document.getElementById("check-audio").addEventListener("click", getWord)
+const btnCheckAudio = document.getElementById("btn-check-audio")
+btnCheckAudio.addEventListener("click", getWord)
