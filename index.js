7cad557afa6b9ba089ff51b005262e1cfe346c76
Adrian Lugo <misteralugo@gmail.com>
2021-12-14 15:37:54 -0600

    script get word and check audio

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // The purpose of this game is to showcase my abilities in front end web development. Specifically, I show my understanding of APIs and capacity to call one in order to make a simple game. Here I am calling the renowned dictionary Merriam Webster's API.
 
-async function getWord() {
-    let response = await fetch("https://www.dictionaryapi.com/api/v3/references/medical/json/abacavir?key=mykey")
+const wordBox = document.getElementById("word-box")
+const btnGetWord = document.getElementById("btn-get-word")
+btnGetWord.addEventListener("click", getWord)
+const btnCheckAudio = document.getElementById("btn-check-audio")
+btnCheckAudio.addEventListener("click", checkAudio)
+
+words = ["abacavir", "abatacept"]
+
+function getWord() {
+    const randomNum = Math.floor(Math.random() * words.length)
+    let randomWord = words[randomNum]
+    wordBox.textContent = randomWord
+    let randomIndex = words.indexOf(randomWord)
+    words.splice(randomIndex, 1)
+    
+    if (words.length === 0) {
+        // End of Game
+    }
+}
+
+async function checkAudio() {
+    let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/medical/json/${wordBox.textContent}?key=mykey`)
     let data = await response.json()
     console.log(data[0].hwi.prs[0].sound.audio)
     const abacavirBaseFilename = data[0].hwi.prs[0].sound.audio
@@ -14,5 +34,3 @@ async function getWord() {
 
 
 
-const btnCheckAudio = document.getElementById("btn-check-audio")
-btnCheckAudio.addEventListener("click", getWord)
