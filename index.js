c6a8980bfb3b11cee33b062fe36427fa79196ae4
Adrian Lugo <misteralugo@gmail.com>
2021-12-14 18:53:44 -0600

    script get word, check audio, and increase score

--- index.js
+++ index.js
@@ -5,23 +5,32 @@ const btnGetWord = document.getElementById("btn-get-word")
 btnGetWord.addEventListener("click", getWord)
 const btnCheckAudio = document.getElementById("btn-check-audio")
 btnCheckAudio.addEventListener("click", checkAudio)
+const btnIncreaseScore = document.getElementById("btn-increase-score")
+btnIncreaseScore.addEventListener("click", increaseScore)
+const playerScore = document.getElementById("player-score")
+let score = 0
 
-words = ["abacavir", "abatacept"]
+words = ["abacavir", "abatacept", "abciximab", "Acanthocheilonema", "accoucheuse", "acetabuloplasty", "acrocephalosyndactyly", "amaurosis", "androstenedione", "anoesis", "antifluoridationist", "antileishmanial", "antiricin", "arytenoepiglottic"]
 
 function getWord() {
     const randomNum = Math.floor(Math.random() * words.length)
     let randomWord = words[randomNum]
-    wordBox.textContent = randomWord
+    if (typeof(randomWord) === "undefined") {
+        randomWord = "End of Game"
+        btnGetWord.style.disabled = "true"
+        btnCheckAudio.style.disabled = "true"
+        btnIncreaseScore.style.disabled = "true"
+    }
+    wordBox.innerHTML = `
+    <p id="random-word">${randomWord}</p>
+    `
     let randomIndex = words.indexOf(randomWord)
     words.splice(randomIndex, 1)
-    
-    if (words.length === 0) {
-        // End of Game
-    }
 }
 
 async function checkAudio() {
-    let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/medical/json/${wordBox.textContent}?key=mykey`)
+    const randomWord = document.getElementById("random-word")
+    let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/medical/json/${randomWord.textContent}?key=mykey`)
     let data = await response.json()
     console.log(data[0].hwi.prs[0].sound.audio)
     const abacavirBaseFilename = data[0].hwi.prs[0].sound.audio
@@ -29,7 +38,11 @@ async function checkAudio() {
     let sound = new Audio(`https://media.merriam-webster.com/audio/prons/en/us/mp3/a/${abacavirBaseFilename}.mp3`)
     console.log(sound.src)
     sound.play()
+}
 
+function increaseScore() {
+    score++
+    playerScore.textContent = `Score: ${score}`
 }
 
 
